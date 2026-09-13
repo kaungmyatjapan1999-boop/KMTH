90cd3830b14673ff2edf8973753c4297f4f2af50
kaungmyatjapan1999-boop <kaungmyatjapan1999@gmail.com>
2026-09-13 19:40:03 +0700

    Implement MyVpnService for VPN functionality

---
+++ app/src/main/java/com/kmth/vpn/MyVpnService.kt
@@ -0,0 +1,39 @@
+package com.kmth.vpn
+
+import android.content.Intent
+import android.net.VpnService
+import android.os.IBinder
+
+class MyVpnService : VpnService() {
+
+    private var vpnInterface: ParcelFileDescriptor? = null
+
+    override fun onStartCommand(
+        intent: Intent?,
+        flags: Int,
+        startId: Int
+    ): Int {
+
+        if (vpnInterface == null) {
+            val builder = Builder()
+
+            builder.setSession("KMTH VPN")
+                .addAddress("10.0.0.2", 32)
+                .addRoute("0.0.0.0", 0)
+
+            vpnInterface = builder.establish()
+        }
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        vpnInterface?.close()
+        vpnInterface = null
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent): IBinder? {
+        return super.onBind(intent)
+    }
+}
